02a6f6e434109a86c3f166c744729e4097872e2b
CeAlCuadrado <cealcuadrado@outlook.com>
2021-08-18 10:29:24 -0400

    Apariencia de tarjetas a usar

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
+  atributos = [
+    { nombre: 'Audiencias pendientes', cantidad: 11 },
+    { nombre: 'Causas litigantes', cantidad: 45 },
+    { nombre: 'Genchi servicio de gendarmería', cantidad: 0 },
+    { nombre: 'Incompetencias especiales', cantidad: 20 },
+    { nombre: 'Medidas cautelares vigentes', cantidad: 15 },
+    { nombre: 'Multas vencidas / pendientes', cantidad: 32 },
+    { nombre: 'Notificaciones pendientes', cantidad: 25 },
+    { nombre: 'Órdenes de detención vigentes', cantidad: 444 },
+    { nombre: 'Sentencias', cantidad: 8 },
+    { nombre: 'Situación de libertad', cantidad: 4 },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
